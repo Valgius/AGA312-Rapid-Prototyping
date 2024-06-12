15251310db503760499226fc4a53a473a8803c4f
Valgius <jax.skinner@hotmail.com>
2024-06-12 12:25:17 +1000

    Lesson 4.4

--- Assets/Prototype 1/Scripts/Enemy.cs
+++ Assets/Prototype 1/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour
     public float speed;
     private Rigidbody enemyRb;
     private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +23,7 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10) { Destroy(gameObject); }
     }
 }
