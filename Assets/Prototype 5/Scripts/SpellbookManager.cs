31b9e8f7ac6e8f814150c42a68e917728d45e5a8
Valgius <jax.skinner@hotmail.com>
2024-08-14 16:30:42 +1000

    Prototype 5: Added Assets. Started Mechanics

---
+++ Assets/Prototype 5/Scripts/SpellbookManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class SpellbookManager : MonoBehaviour
+{
+    public GameObject spellbook;
+    public GameObject barrier;
+    public GameObject spellCircle;
+
+    public GameObject spellbookPanel;
+    private bool spellbokOpen;
+    public GameObject endGamePanel;
+
+    public int RunesActivated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spellbokOpen = false;
+        spellbook.SetActive(false);
+        spellbookPanel.SetActive(false);
+        spellCircle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            TriggerSpellBook();
+    }
+
+    public void TriggerSpellBook()
+    {
+        spellbokOpen = !spellbokOpen;
+        spellbookPanel.SetActive(spellbokOpen);
+    }
+
+}
